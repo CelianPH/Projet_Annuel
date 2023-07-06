e55c32923fadc2bad2b4b34ed545e6fd885e609e
Célian Philibert <celian.philibert@gmail.com>
2023-07-06 16:50:35 +0200

    celian: front requete api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Liste des étudiants</h1>
+      <ul>
+        {students.map((etudiant, index) => (
+          <li key={index}>{etudiant.nom}</li>
+        ))}
+      </ul>
     </div>
   );
 }
